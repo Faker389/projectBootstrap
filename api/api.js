7d9945a98e5f7b6da2be597682cfc13224317c74
Faker389 <161638132+Faker389@users.noreply.github.com>
2024-09-22 22:47:16 +0200

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,16 +1,17 @@
 const expres = require("express")
 const cors = require("cors")
 const fs = require("fs")
-const jsonFile = require("./file.json")
 const app = expres()
 app.use(expres.json())
 app.use(cors())
 app.listen(8000)
 app.get("/getJson",(req,res)=>{
+    const jsonFile = require("./file.json")
     res.json(jsonFile)
 })
 app.post("/addJson",(req,res)=>{
     var obj =req.body
+    var jsonFile = require("./file.json")
     jsonFile.push(obj)
     fs.writeFile("./file.json",JSON.stringify(jsonFile,null,2),'utf-8',(err)=>{
         if(err){
